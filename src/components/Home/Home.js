671dd70bbbc4cd41f9bdcccc1031b11c53ffa386
codernoyon <noyonrahman2003@gmail.com>
2022-04-05 17:19:02 +0600

    readme file re edite

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='w-5/6 mx-auto py-10 pb-20'>
-                <h2 className="text-4xl text-center section-title mb-5">Customer Reviews</h2>
+                <h2 className="text-4xl text-center section-title mb-10">Customer Reviews</h2>
                 <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-7 mb-8'>
                     {
                         reviews.slice(0, 3).map(review => <Review
